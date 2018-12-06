95a3ff9c01a9f32a1dcdc68d92c426566dab8907
Pavel Fedorov <catkiller95@gmail.com>
2018-12-06 03:01:31 +0300

    Осталось сделать перебор всех по всему (итеративный)

--- lab1/main.c
+++ lab1/main.c
@@ -39,94 +39,62 @@ struct scheduleLine {
     int teacher;
 };
 
-struct group *groups;
-int *disciplines;
-struct teacher *teachers;
-int *locations;
+int groups[GROUP_COUNT];
+int disciplines[DISCIP_COUNT];
+int teacher [TEACHER_COUNT];
+int locations [LOCATION_COUNT];
+int plans[GROUP_COUNT][DISCIP_COUNT];
+int employments[TEACHER_COUNT][DISCIP_COUNT];
 
-void destroyInput() {
-    free(groups);
-    free(disciplines);
-    free(teachers);
-    free(locations);
-}
-
-void initInput()
-{
-    groups = (struct group *)calloc(GROUP_COUNT, sizeof(struct group));
-    if (!groups) {
-        printf("Fail to init groups");
-        exit(-1);
-    }
+void init() {
     for (int I = 0; I < GROUP_COUNT; I++) {
-        groups[I].name = I;
-        groups[I].plans = (struct plan *)calloc(DISCIP_COUNT, sizeof(struct plan));
-        if (!groups[I].plans) {
-            destroyInput();
-            printf("Fail to init plan for group %d", I);
-            exit(-2);
-        }
-
-        for (int J = 0; J < DISCIP_COUNT; I++) {
-            groups[I].plans[J].count = 1;
-            groups[I].plans[J].discipline = J;
+        groups[I] = I;
+        for (int J = 0; J < DISCIP_COUNT; J++) {
+            plans[I][J] = 1;
         }
     }
 
-    disciplines = calloc(DISCIP_COUNT, sizeof(int));
-    if (!disciplines) {
-        printf("Fail to init disciplines");
-        destroyInput();
-        exit(-3);
-    }
     for (int I = 0; I < DISCIP_COUNT; I++) {
         disciplines[I] = I;
     }
 
-    teachers = (struct teacher*)calloc(TEACHER_COUNT, sizeof(struct teacher));
-    if (!teachers) {
-        printf("Fail to init teachers");
-        destroyInput();
-        exit(-4);
-    }
     for (int I = 0; I < TEACHER_COUNT; I++) {
-        teachers[I].name = I;
-        teachers[I].employment = (struct teacherEmployment *)calloc(DISCIP_COUNT, sizeof(struct teacherEmployment));
-        if (!teachers[I].employment) {
-            printf("Fail to init teacher %d employment", I);
-            destroyInput();
-            exit(-4);
-        }
+        teacher[I] = I;
         for (int J = 0; J < DISCIP_COUNT; J++) {
-            teachers
+            employments[I][J] = 1;
         }
     }
 
-    locations = calloc(LOCATION_COUNT, sizeof(int));
-    if (!locations) {
-        printf("Fail to init locations");
-        destroyInput();
-        exit(-5);
-    }
     for (int I = 0; I < LOCATION_COUNT; I++) {
         locations[I] = I;
     }
 
-    plan = (struct line *)calloc(DISCIP_COUNT, sizeof(struct line));
-    if (!plan) {
-        printf("Fail to init plan");
-        destroyInput();
-        exit(-6);
-    }
-    for (int I = 0; I < DISCIP_COUNT; I++) {
-        plan[I].count = 1;
-        plan[I].discipline = I;
-    }
-
-    // teacherLoads = (struct load *)calloc()
-
-    printf("Success init input data");
     return;
+}
+
+int findLocation(struct scheduleLine **schedules, int location, int lesson) {
+    int res = 0;
+    for (int I = 0; I < GROUP_COUNT; I++) {
+        if (schedules[I][lesson].location == location)
+            res++;
+        
+        if (res >= 2)
+            break;
+    }
+    return res;
+}
+
+int findTeacher(struct scheduleLine **schedules, int teacher, int lesson) {
+    int res = 0;
+    for (int I = 0; I < GROUP_COUNT; I++) {
+        if (schedules[I][lesson].teacher == teacher) 
+            res++;
+
+        if (res >= 2) {
+            break;
+        }
+    }
+    return res;
 }
 
 int verifyResolve(struct scheduleLine **schedules) {
@@ -161,44 +129,60 @@ int verifyResolve(struct scheduleLine **schedules) {
                 }
             }
 
+            int teacherLocation = findTeacher(schedules, schedules[I][J].teacher, J);
+            if (teacherLocation >= 2)
+                return -1;
+
+            int locationState = findLocation(schedules, schedules[I][J].location, J);
+            if (locationState >= 2) {
+                return -1;
+            }
+        }
+
+        for (int J = 0; J < TEACHER_COUNT; J++) {
+            int cur_teacher = J;
+            bool state = false;
+            int count = 0;
+            for (int K = 0; K < MAX_OCCUP_IN_DAY; K++) {
+                int res = findTeacher(schedules, cur_teacher, K);
+                if (res == 1)
+                    count++;
+
+                if (res == 1 && !state) {
+                    state == true;
+                }
+
+                if (res == 1 && state) {
+                    res++;
+                }
+
+                if (res > MAX_OCCUP_OF_TEACH)
+                    return -1;
+            }
         }
     }
 }
 
 void direct()
 {
-    struct scheduleLine **schedules = (struct scheduleLine **)calloc(sizeof(struct scheduleLine *), GROUP_COUNT);
-    if (!schedules) {
-        printf("Fail to init schedules");
-        destroyInput();
-        exit(-7);
-    }
+    struct scheduleLine schedules[GROUP_COUNT][MAX_OCCUP_IN_DAY];
     for (int I = 0; I < GROUP_COUNT; I++) {
-        schedules[I] = (struct scheduleLine *)calloc(sizeof(struct scheduleLine), MAX_OCCUP_IN_DAY);
-        if (!schedules[I]) {
-            printf("Fail to init schedules by %d group", I);
-            destroyInput();
-            exit(-8);
+        for (int J = 0; J < MAX_OCCUP_IN_DAY; J++) {
+            schedules[I][J].state = freeLesson;
+            schedules[I][J].discipline = -1;
+            schedules[I][J].location = -1;
+            schedules[I][J].teacher = -1;
         }
     }
 
-    printf("Success create schedules");
+    //  перебор
 
-
-
-    printf("Free scheduls");
-    for (int I = 0; I < GROUP_COUNT; I++) {
-        free(schedules[I]);
-    }
-
-    free(schedules);
     return;
 }
 
 void main()
 {
-    initInput();
+    init();
     direct();
-    destroyInput();
     return;
 }
